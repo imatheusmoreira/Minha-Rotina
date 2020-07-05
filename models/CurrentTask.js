043aa9f20e6ea0663386030ea4847753760ac053
Matheus Moreira <imatheusmoreira@gmail.com>
2020-07-05 02:55:06 -0300

    Fix: Current task contará os dias. Feat: reminder

--- models/CurrentTask.js
+++ models/CurrentTask.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const currentTasksSchema = new mongoose.Schema({
     task_id: { type: String, required: true }, //Task ID
     user_uid: { type: String, required: true }, //UID
-    expected_time: { type: Number },
-    completed_time: { type: Number, required: false, default: 0 },
+    expected_days: { type: Number },
+    completed_days: { type: Number, required: false, default: 0 },
     created_at: { type: Date, default: Date.now }
 });
 
--- models/Task.js
+++ models/Task.js
@@ -5,12 +5,13 @@ const taskSchema = new mongoose.Schema({
     name: { type: String, required: true },
     category_id: { type: String, required: true, default: 0 },
     finished: { type: Boolean, required: true, default: false },
-    status: { type: Boolean, required: true, default: false },
+    status: { type: Boolean, required: true, default: false },//Active or not
     days_of_week: { type: Array, required: true, "default": [] },//Put day 0 to Domingo, 1 to Monday...
     start_date: { type: Date, required: true },
     end_date: { type: Date, required: true },
     start_time: { type: String, required: true },
     end_time: { type: String, required: true },
+    reminder: { type: number, required: true, default: 0 },
     created_at: { type: Date, default: Date.now }
 });
 
